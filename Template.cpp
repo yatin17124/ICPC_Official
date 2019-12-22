53ed62360c398323f356949ff8ed45cdd442a9e7
yatin17124 <45530861+yatin17124@users.noreply.github.com>
2019-12-22 14:48:05 +0530

    Add files via upload

---
+++ Template.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp> 
+
+using namespace std;
+using namespace __gnu_pbds; 
+
+#define int long long
+#define pii pair<int,int>
+#define pb push_back
+#define f first
+#define s second
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+int power(int x, int y, int mod = 2e18){
+	int ans = 1; x%= mod;
+	while(y){
+		if(y&1) ans = (x * ans) % mod;
+		x = (x * x) % mod; y>>=1;
+	}
+	return ans;
+}
+typedef tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+// s.order_of_key(key)
+
+int const MOD = 1e9+7;
+int const inf = 2e18;
+int const N = 2e5+10;
+
+signed main() {
+	IOS;
+
+return 0;
+}
