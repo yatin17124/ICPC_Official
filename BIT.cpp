3eff82df1168220169cd57902884f2882c456b68
yatin17124 <45530861+yatin17124@users.noreply.github.com>
2019-12-24 15:46:41 +0530

    Update BIT.cpp

--- BIT.cpp
+++ BIT.cpp
@@ -1,14 +1,11 @@
-struct BIT
-{
+struct BIT{
 	int N;
 	vector<int> bit;
-
 	void init(int n)
 	{
 		N = n;
 		bit.assign(n + 1, 0);
 	}
-
 	void update(int idx, int val)
 	{
 		while(idx <= N)
@@ -17,35 +14,28 @@ struct BIT
 			idx += idx & -idx;
 		}
 	}
-
 	void updateMax(int idx, int val)
 	{
 		while(idx <= N)
-		{
-			bit[idx] = max(bit[idx], val);
+		{	bit[idx] = max(bit[idx], val);
 			idx += idx & -idx;
 		}
 	}
-
 	int pref(int idx)
 	{
 		int ans = 0;
 		while(idx > 0)
-		{
-			ans += bit[idx];
+		{	ans += bit[idx];
 			idx -= idx & -idx;
 		}
 		return ans;
 	}
-
 	int rsum(int l, int r)
 	{
 		return pref(r) - pref(l - 1);
 	}
-
 	int prefMax(int idx)
-	{
-		int ans = -2e9;
+	{	int ans = -2e9;
 		while(idx > 0)
 		{
 			ans = max(ans, bit[idx]);
